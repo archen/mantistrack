a2ed24a2c50bbb928fa3d20f46cfdfc00c41cf72
Kevin Glavin <archen.sol@gmail.com>
2014-02-10 20:18:12 -0500

    trivial

--- mantistrack/mantises/views.py
+++ mantistrack/mantises/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from django.core.urlresolvers import reverse
 from django.db.utils import IntegrityError
 from django.views.generic.edit import CreateView, UpdateView
 
